c18a670d98632459ce03e6cf3f84ae7c95f93730
loganvaz <loganvaz03@gmail.com>
2022-12-16 13:56:40 -0600

    adding

--- scripts/load_registration.js
+++ scripts/load_registration.js
@@ -1,12 +1,20 @@
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
+window.onload = () => {
+    console.log("begin\n");
+    console.log("window loaded\n");
+    document.getElementById("sampleButton").click();
+    console.log("window done\n");
+}
 
 let runDriver = () => {
-    
+    /* 
     let {Builder} = require('selenium-webdriver');
     let chrome = require('selenium-webdriver/chrome');
 
     const service = new chrome.ServiceBuilder('/path/to/chromedriver');
     const driver = new Builder().forBrowser('chrome').setChromeService(service).build();
+    */
 
     //const webdriver = require('selenium-webdriver');
     //const chrome = require('selenium-webdriver/chrome');

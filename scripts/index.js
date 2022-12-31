a1747b87eaf48842806da1f2967740358d77c947
loganvaz <loganvaz03@gmail.com>
2022-12-30 18:02:45 -0600

    got popup looking slightly better

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,43 @@ document.addEventListener('DOMContentLoaded', function(){
     const class_queue = ["51895","13220"];
     //read list/hashmap of course codes that we want to waitlist
     // chrome.storage.session.setAccessLevel({"accessLevel": "TRUSTED_AND_UNTRUSTED_CONTEXTS"});
+    
     chrome.storage.session.set({ "class_queue": class_queue }).then(() => {
         //click the "submit" button to enter into registration section
         var manage_button = document.getElementById("manage-button");
         manage_button.addEventListener('click', open_manage_registration);
     });
+    
 
     //display current registration time
     chrome.storage.sync.get(["global_alarm"], function(data) {
         if(data.global_alarm == undefined){
             document.getElementById("registration-time").style.display = "none";
+            document.getElementById("registration-time-container").style.marginBottom = '1.5em';
             document.getElementById("no-registration-time").style.removeProperty("display");
         } else {
             set_displayed_time(data.global_alarm);
         }
     });
+
+
+
+    //add list of classes
+    chrome.storage.sync.get(["saved_registration"], function(data) {
+        if(data.saved_registration == undefined){
+            document.getElementById("class-list-div").hidden = false;
+        } else {
+            let list_of_classes = document.getElementById("class-list");
+            data["saved_registration"].forEach((instance) => {
+                let newest = document.createElement("div");
+                newest.classList.add("centered");
+                newest.classList.add("course_element");
+                newest.innerText = instance["Course name"] + " (" + instance["Course code"] + ")";
+                list_of_classes.appendChild(newest);
+            })
+        }
+    });
+
 
 });
 
